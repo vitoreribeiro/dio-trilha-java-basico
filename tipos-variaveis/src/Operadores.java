375ae5523124b259a6f7c57cb0d5baee188bf8be
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2022-09-12 20:37:31 -0300

    atualiazacao do curso - novas aulas realizadas

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+        
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if (condicao1 && 7 > 4) {
+            System.out.println("as duas condicoes sao verdadeiras");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("uma das condicoes e verdadeira");
+        }
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,28 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estudem a classe String que representa texto na aplicação
+
+
+        String meuNome = "VITOR RIBEIRO";
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        // CASTING
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(meuNome);
+        System.out.println(salarioMinimo);
+        System.out.println(numeroCurto2);
+        System.out.println(VALOR_DE_PI);
     }
 }
