9f8945d36b4b4c43d40c363e4b5096d738f9ea7f
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2022-09-15 22:53:08 -0300

    formacao java developer DIO - comandos no terminal

---
+++ terminal-e-argumentos-java/src/AboutMe.java
@@ -0,0 +1,13 @@
+public class AboutMe {
+    public static void main(String[] args) throws Exception {
+        
+        String nome= args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+    }
+}
---
+++ terminal-e-argumentos-java/src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse{
+    public static void main(String[] args) {
+    System.out.println("Oi Turma, fui executado pelo Terminal");
+    }
+
+}
